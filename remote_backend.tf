b8e4ff96dda1b43776a70e2e7ae1e16b8e7e193c
AggieEngineer2k <aggieengineer2k@hotmail.com>
2023-02-07 21:23:21 +0000

    Added network module

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "cpc-brownjl"
     workspaces {
       name = "hashicat-azure"
